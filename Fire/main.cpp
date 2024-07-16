446b93e35cd762dc714fb59cdcfec9fadec66551
unknown <2236872724@qq.com>
2024-07-16 18:55:59 +0800

    改动了飞机类型，飞机选人，敌机类型，计分板

--- Fire/main.cpp
+++ Fire/main.cpp
@@ -156,6 +156,11 @@ public:
 		}
 	}
 
+	void setHP(int times) {
+		if (times == 0) return;
+		HP *= times;
+	}
+
 	bool hurt()
 	{
 		HP--;
@@ -164,9 +169,12 @@ public:
 
 	RECT& GetRect() { return rect; }
 
+	unsigned int speed;
+
 private:
 	IMAGE& img;
 	RECT rect;
+
 
 	unsigned int HP;
 
@@ -175,8 +183,8 @@ private:
 class Enemy
 {
 public:
-	Enemy(IMAGE& img, int x, IMAGE*& boom)
-		:img(img), isdie(false), boomsum(0)
+	Enemy(IMAGE& img, int x, IMAGE*& boom,int type = 1)
+		:img(img), isdie(false), boomsum(0),EnemyType(type)
 	{
 		selfboom[0] = boom[0];
 		selfboom[1] = boom[1];
@@ -207,6 +215,22 @@ public:
 		}
 		rect.top += 4;
 		rect.bottom += 4;
+		if (EnemyType == 3) {
+			if (direction == 0) { // 左右移动
+				rect.left += 2; // 向右移动
+				if (rect.left > swidth - img.getwidth()) {
+					direction = 1; // 到达右边界，改变方向
+				}
+			}
+			else {
+				rect.left -= 2; // 向左移动
+				if (rect.left < 0) {
+					direction = 0; // 到达左边界，改变方向
+				}
+			}
+		}
+
+		rect.right = rect.left + img.getwidth();
 		putimage(rect.left, rect.top, &img);
 
 		return true;
@@ -217,12 +241,28 @@ public:
 		isdie = true;
 	}
 
+	void MoveTowardsHero(Hero& player) {
+		if (player.GetRect().top < rect.top) {
+			rect.top += 2; // 向玩家位置靠近
+		}
+		if (player.GetRect().left < rect.left) {
+			rect.left -= 2; // 向玩家位置靠近
+		}
+		else {
+			rect.left += 2;
+		}
+	}
+
+
 	RECT& GetRect() { return rect; }
+	int getEnemyType() { return EnemyType; }
 
 private:
 	IMAGE& img;
 	RECT rect;
 	IMAGE selfboom[3];
+	int direction = 0;
+	int EnemyType;
 
 	bool isdie;
 	int boomsum;
@@ -284,9 +324,9 @@ public:
 };
 
 
-bool AddEnemy(vector<Enemy*>& es, IMAGE& enemyimg, IMAGE* boom)
+bool AddEnemy(vector<Enemy*>& es, IMAGE& enemyimg, IMAGE* boom,int type)
 {
-	Enemy* e = new Enemy(enemyimg, abs(rand()) % (swidth - enemyimg.getwidth()), boom);
+	Enemy* e = new Enemy(enemyimg, abs(rand()) % (swidth - enemyimg.getwidth()), boom, type);
 	for (auto& i : es)
 	{
 		if (RectDuangRect(i->GetRect(), e->GetRect()))
@@ -300,7 +340,81 @@ bool AddEnemy(vector<Enemy*>& es, IMAGE& enemyimg, IMAGE* boom)
 
 }
 
-bool Play()
+void DrawScoreBoard(unsigned long long kill)
+{
+	TCHAR str[128];
+	_stprintf_s(str, 128, _T("击杀数：%llu"), kill);
+
+	settextcolor(BLACK);
+	settextstyle(20, 0, _T("黑体"));
+	outtextxy(10, 10, str); // 在窗口左上角绘制
+}
+
+void ChooseHero(IMAGE& hero1, IMAGE& hero2, IMAGE& hero3,int& plainType)
+{
+	LPCTSTR title = _T("选择战机");
+	RECT hero1Rect, hero2Rect, hero3Rect;
+
+	BeginBatchDraw();
+	setbkcolor(WHITE);
+	cleardevice();
+	settextstyle(60, 0, _T("黑体"));
+	settextcolor(BLACK);
+	outtextxy(swidth / 2 - textwidth(title) / 2, sheight / 5, title);
+
+	// 绘制战机选项
+	hero1Rect.left = swidth / 4 - hero1.getwidth() / 2;
+	hero1Rect.top = sheight / 2 - hero1.getheight() / 2;
+	hero1Rect.right = hero1Rect.left + hero1.getwidth();
+	hero1Rect.bottom = hero1Rect.top + hero1.getheight();
+
+	hero2Rect.left = swidth / 2 - hero2.getwidth() / 2;
+	hero2Rect.top = hero1Rect.top;
+	hero2Rect.right = hero2Rect.left + hero2.getwidth();
+	hero2Rect.bottom = hero2Rect.top + hero2.getheight();
+
+	hero3Rect.left = swidth * 3 / 4 - hero3.getwidth() / 2;
+	hero3Rect.top = hero1Rect.top;
+	hero3Rect.right = hero3Rect.left + hero3.getwidth();
+	hero3Rect.bottom = hero3Rect.top + hero3.getheight();
+
+	putimage(hero1Rect.left, hero1Rect.top, &hero1);
+	putimage(hero2Rect.left, hero2Rect.top, &hero2);
+	putimage(hero3Rect.left, hero3Rect.top, &hero3);
+
+	EndBatchDraw();
+
+	// 等待选择
+	while (true)
+	{
+		ExMessage mess;
+		getmessage(&mess, EM_MOUSE);
+		if (mess.lbutton)
+		{
+			if (PointInRect(mess.x, mess.y, hero1Rect))
+			{
+				// 选择战机1
+				plainType = 1;
+				return;
+			}
+			else if (PointInRect(mess.x, mess.y, hero2Rect))
+			{
+				// 选择战机2
+				plainType = 2;
+				return;
+			}
+			else if (PointInRect(mess.x, mess.y, hero3Rect))
+			{
+				// 选择战机3
+				plainType = 3;
+				return;
+			}
+		}
+	}
+}
+
+
+bool Play(int plainType)
 {
 	setbkcolor(WHITE);
 	cleardevice();
@@ -309,14 +423,14 @@ bool Play()
 	IMAGE heroimg, enemyimg, bkimg, bulletimg;
 	IMAGE eboom[3];
 
-	loadimage(&heroimg, _T("../images/me1.png"));
-	loadimage(&enemyimg, _T("../images/enemy1.png"));
-	loadimage(&bkimg, _T("../images/bk2.png"), swidth, sheight * 2);
-	loadimage(&bulletimg, _T("../images/bullet1.png"));
+	loadimage(&heroimg, _T("../../images/me1.png"));
+	loadimage(&enemyimg, _T("../../images/enemy1.png"));
+	loadimage(&bkimg, _T("../../images/bk2.png"), swidth, sheight * 2);
+	loadimage(&bulletimg, _T("../../images/bullet1.png"));
 
-	loadimage(&eboom[0], _T("../images/enemy1_down2.png"));
-	loadimage(&eboom[1], _T("../images/enemy1_down3.png"));
-	loadimage(&eboom[2], _T("../images/enemy1_down4.png"));
+	loadimage(&eboom[0], _T("../../images/enemy1_down2.png"));
+	loadimage(&eboom[1], _T("../../images/enemy1_down3.png"));
+	loadimage(&eboom[2], _T("../../images/enemy1_down4.png"));
 
 	BK bk = BK(bkimg);
 	Hero hp = Hero(heroimg);
@@ -332,13 +446,18 @@ bool Play()
 
 	for (int i = 0; i < 5; i++)
 	{
-		AddEnemy(es, enemyimg, eboom);
+		AddEnemy(es, enemyimg, eboom, i%3+1);
 	}
-
+	unsigned int speed = plainType;
+	hp.setHP(plainType);
 	while (is_play)
 	{
 		bsing++;
-		if (bsing % 10 == 0)
+
+
+
+		
+		if (bsing %  speed == 0)
 		{
 			bs.push_back(new Bullet(bulletimg, hp.GetRect()));
 		}
@@ -358,7 +477,8 @@ bool Play()
 		flushmessage();
 		Sleep(2);
 		hp.Control();
-
+		// 绘制计分板
+		DrawScoreBoard(kill);
 		if (_kbhit())
 		{
 			char v = _getch();
@@ -417,8 +537,12 @@ bool Play()
 		}
 
 		auto it = es.begin();
+		
 		while (it != es.end())
 		{
+			if ((*it)->getEnemyType() == 2) {
+				(*it)->MoveTowardsHero(hp);
+			}
 			if (RectDuangRect((*it)->GetRect(), hp.GetRect()))
 			{
 				if (clock() - hurtlast >= hurttime)
@@ -452,7 +576,7 @@ bool Play()
 		}
 		for (int i = 0; i < 5 - es.size(); i++)
 		{
-			AddEnemy(es, enemyimg, eboom);
+			AddEnemy(es, enemyimg, eboom, i%3+1);
 		}
 
 		EndBatchDraw();
@@ -467,12 +591,20 @@ int main()
 {
 	// easyx初始化
 	initgraph(swidth, sheight, EW_NOMINIMIZE | EW_SHOWCONSOLE);
+
+	IMAGE hero1, hero2, hero3;
+	loadimage(&hero1, _T("../../images/me1.png"));
+	loadimage(&hero2, _T("../../images/me1.png"));
+	loadimage(&hero3, _T("../../images/me1.png"));
 	bool is_live = true;
+
+	int plainType = 1;
 	while (is_live)
 	{
 		Welcome();
+		ChooseHero(hero1, hero2, hero3,plainType);
 		// Play
-		is_live = Play();
+		is_live = Play(plainType);
 	}
 
 	return 0;
--- README.md
+++ README.md
@@ -4,10 +4,8 @@ vs版本2022
 
 需要EasyX支持
 
-素材在images里，Play函数中loadimage()里的素材路径需要自己改一下哦~
+素材在images里，如果程序跑不通Play函数中loadimage()里的素材路径需要自己改
 
-界面美化、功能添加欢迎 pull requests！
+添加了计分板，多种飞机选择，敌机逻辑。
 
-另一个新手项目[FlappyBird](https://github.com/SCTOSOP/FlappyBird_BC)
-
-**作者最近在忙[这个项目](https://github.com/SCTOSOP/MaL_new)，快来围观（doge）**
+（写的依托QAQ）
